ebf129daa7fa08123b8f296868da51b68e6e7973
Jerex Lennon <skyguyverph@gmail.com>
2020-11-14 14:00:18 +0800

    Added HLS and Dash Trans

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const NodeMediaServer = require('node-media-server');
 //Imports the Google Cloud client library
-const {PubSub} = require('@google-cloud/pubsub');
+const { PubSub } = require('@google-cloud/pubsub');
 
 const getStreamSlug = (streamPath) => {
-        let parts = streamPath.split('/');
-        return parts[parts.length - 1];
+  let parts = streamPath.split('/');
+  return parts[parts.length - 1];
 };
 
 const NodePubSub = async (streamId, msg, streamPath, args) => {
@@ -13,48 +13,48 @@ const NodePubSub = async (streamId, msg, streamPath, args) => {
    * TODO(developer): Uncomment these variables before running the sample.
    */
   const projectId = 'portgrace-backend-277111';
-   const topicName = 'projects/portgrace-backend-277111/topics/live-streaming';
-   const data = JSON.stringify(msg);  
+  const topicName = 'projects/portgrace-backend-277111/topics/live-streaming';
+  const data = JSON.stringify(msg);
 
-    // Creates a client; cache this for further use
-    const pubSubClient = new PubSub({projectId});
+  // Creates a client; cache this for further use
+  const pubSubClient = new PubSub({ projectId });
 
-    async function publishMessageWithCustomAttributes() {
+  async function publishMessageWithCustomAttributes() {
     // Publishes the message as a string, e.g. "Hello, world!" or JSON.stringify(someObject)
     const dataBuffer = Buffer.from(data);
 
     // Add two custom attributes, origin and username, to the message
     const customAttributes = {
-        streamId: streamId,
-        args: JSON.stringify(args),
-        streamPath: streamPath,
-        slug: getStreamSlug(streamPath)
+      streamId: streamId,
+      args: JSON.stringify(args),
+      streamPath: streamPath,
+      slug: getStreamSlug(streamPath)
     };
 
     const messageId = await pubSubClient
       .topic(topicName)
       .publish(dataBuffer, customAttributes);
-       console.log(`Message ${messageId} published.`);
+    console.log(`Message ${messageId} published.`);
   }
 
   publishMessageWithCustomAttributes().catch(console.error);
   // [END pubsub_publish_custom_attributes]
 }
 
-  const config = {
+const config = {
   rtmp: {
     port: 1935,
     chunk_size: 60000,
     gop_cache: true,
     ping: 30,
     ping_timeout: 60,
-	/*
-    ssl: {
-      port: 443,
-      key: './privatekey.pem',
-      cert: './certificate.pem',
-    }
-	*/
+    /*
+      ssl: {
+        port: 443,
+        key: './privatekey.pem',
+        cert: './certificate.pem',
+      }
+    */
   },
   http: {
     port: 8000,
@@ -76,6 +76,18 @@ const NodePubSub = async (streamId, msg, streamPath, args) => {
     publish: false,
     secret: 'nodemedia2017privatekey'
   },
+  trans: {
+    ffmpeg: '/usr/bin/ffmpeg',
+    tasks: [
+      {
+        app: 'live',
+        hls: true,
+        hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
+        dash: true,
+        dashFlags: '[f=dash:window_size=3:extra_window_size=5]'
+      }
+    ]
+  }
   // relay: {
   //   ffmpeg: 'C:/ffmpeg/bin/ffmpeg.exe',
   //   tasks: [
@@ -88,50 +100,50 @@ const NodePubSub = async (streamId, msg, streamPath, args) => {
   // }
 };
 
-  let nms = new NodeMediaServer(config)
-  nms.run();
+let nms = new NodeMediaServer(config)
+nms.run();
 
-  nms.on('preConnect', (id, args) => {
-    console.log('[NodeEvent on preConnect]', `id=${id} args=${JSON.stringify(args)}`);
-    // let session = nms.getSession(id);
-    // session.reject();
-  });
+nms.on('preConnect', (id, args) => {
+  console.log('[NodeEvent on preConnect]', `id=${id} args=${JSON.stringify(args)}`);
+  // let session = nms.getSession(id);
+  // session.reject();
+});
 
-  nms.on('postConnect', (id, args) => {
-    console.log('[NodeEvent on postConnect]', `id=${id} args=${JSON.stringify(args)}`);
-  });
+nms.on('postConnect', (id, args) => {
+  console.log('[NodeEvent on postConnect]', `id=${id} args=${JSON.stringify(args)}`);
+});
 
-  nms.on('doneConnect', (id, args) => {
-    console.log('[NodeEvent on doneConnect]', `id=${id} args=${JSON.stringify(args)}`);
-  });
+nms.on('doneConnect', (id, args) => {
+  console.log('[NodeEvent on doneConnect]', `id=${id} args=${JSON.stringify(args)}`);
+});
 
-  nms.on('prePublish', (id, StreamPath, args) => {
-    console.log('[NodeEvent on prePublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
-    // let session = nms.getSession(id);
-    // session.reject();
-    NodePubSub(id, 'prePublish', StreamPath, args);
-  });
+nms.on('prePublish', (id, StreamPath, args) => {
+  console.log('[NodeEvent on prePublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
+  // let session = nms.getSession(id);
+  // session.reject();
+  NodePubSub(id, 'prePublish', StreamPath, args);
+});
 
-  nms.on('postPublish', (id, StreamPath, args) => {
-    console.log('[NodeEvent on postPublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
-    NodePubSub(id, 'postPublish', StreamPath, args);
-  });
+nms.on('postPublish', (id, StreamPath, args) => {
+  console.log('[NodeEvent on postPublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
+  NodePubSub(id, 'postPublish', StreamPath, args);
+});
 
-  nms.on('donePublish', (id, StreamPath, args) => {
-    console.log('[NodeEvent on donePublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
-    NodePubSub(id, 'donePublish', StreamPath, args);
-  });
+nms.on('donePublish', (id, StreamPath, args) => {
+  console.log('[NodeEvent on donePublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
+  NodePubSub(id, 'donePublish', StreamPath, args);
+});
 
-  nms.on('prePlay', (id, StreamPath, args) => {
-    console.log('[NodeEvent on prePlay]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
-    // let session = nms.getSession(id);
-    // session.reject();
-  });
+nms.on('prePlay', (id, StreamPath, args) => {
+  console.log('[NodeEvent on prePlay]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
+  // let session = nms.getSession(id);
+  // session.reject();
+});
 
-  nms.on('postPlay', (id, StreamPath, args) => {
-    console.log('[NodeEvent on postPlay]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
-  });
+nms.on('postPlay', (id, StreamPath, args) => {
+  console.log('[NodeEvent on postPlay]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
+});
 
-  nms.on('donePlay', (id, StreamPath, args) => {
-    console.log('[NodeEvent on donePlay]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
-  });
+nms.on('donePlay', (id, StreamPath, args) => {
+  console.log('[NodeEvent on donePlay]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
+});
